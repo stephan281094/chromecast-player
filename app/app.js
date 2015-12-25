4e69632960b01b12d7146c6acebc8106bf8cf875
stephan281094 <stephan281094@gmail.com>
2015-12-25 16:23:46 +0100

    Add volume buttons

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var volume = require('./volume');
+
+var raiseVolume = document.querySelector('.raiseVolume');
+var lowerVolume = document.querySelector('.lowerVolume');
+
+raiseVolume.addEventListener('click', volume.raise);
+lowerVolume.addEventListener('click', volume.lower);
---
+++ app/volume.js
@@ -0,0 +1,9 @@
+module.exports = {
+  raise: function() {
+    console.log('raise volume');
+  },
+
+  lower: function() {
+    console.log('lower volume');
+  }
+};
