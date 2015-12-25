91e1a561074b490c1a11db5b091e553368eba7f5
stephan281094 <stephan281094@gmail.com>
2015-12-26 00:47:37 +0100

    Fix code styles

--- app/app.js
+++ app/app.js
@@ -1,13 +1,14 @@
 var player = require('chromecast-player')()
 
-player.attach(function(err, p) {
+player.attach(function (err, p) {
+  if (err) console.error('An error ocurred while attaching the player: ' + err)
   var pauseButton = document.querySelector('.pause')
-  var playButton  = document.querySelector('.play')
+  var playButton = document.querySelector('.play')
 
-  pauseButton.addEventListener('click', function() {
+  pauseButton.addEventListener('click', function () {
     p.pause()
   })
-  playButton.addEventListener('click', function() {
+  playButton.addEventListener('click', function () {
     p.play()
   })
 })
