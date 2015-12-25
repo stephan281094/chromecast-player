1ad868a0ea340dae7b8e360e5309e1a4407e91e7
stephan281094 <stephan281094@gmail.com>
2015-12-26 00:52:28 +0100

    Add stop button

--- app/app.js
+++ app/app.js
@@ -4,11 +4,15 @@ player.attach(function (err, p) {
   if (err) console.error('An error ocurred while attaching the player: ' + err)
   var pauseButton = document.querySelector('.pause')
   var playButton = document.querySelector('.play')
+  var stopButton = document.querySelector('.stop')
 
   pauseButton.addEventListener('click', function () {
     p.pause()
   })
   playButton.addEventListener('click', function () {
     p.play()
+  })
+  stopButton.addEventListener('click', function () {
+    p.stop()
   })
 })
