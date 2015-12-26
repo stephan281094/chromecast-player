cd5b1620622f1dd10b64acc49fca853c15698984
stephan281094 <stephan281094@gmail.com>
2015-12-26 20:23:59 +0100

    Handle errors and fix code styling

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@ var player = require('chromecast-player')()
 player.attach(function (err, p) {
   if (err) console.error('An error ocurred while attaching the player: ' + err)
   var playButton = document.querySelector('.play')
-  var playButtonIcon = document.querySelector('.play i.fa');
+  var playButtonIcon = document.querySelector('.play i.fa')
 
   playButton.addEventListener('click', function () {
     p.getStatus(function (err, status) {
+      if (err) console.error('An error ocurred while getting status of player: ' + err)
       if (status.playerState === 'PLAYING') {
         p.pause(function () {
           playButtonIcon.classList.remove('fa-play')
