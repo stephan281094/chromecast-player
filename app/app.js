483a7f47efb691435acd2756c3602b4aa7ff833c
stephan281094 <stephan281094@gmail.com>
2015-12-29 23:07:30 +0100

    Add working progress

--- app/app.js
+++ app/app.js
@@ -4,7 +4,9 @@ player.attach(function (err, p) {
   if (err) console.error('An error ocurred while attaching the player: ' + err)
   var playButton = document.querySelector('.play')
   var playButtonIcon = document.querySelector('.play i.fa')
+  var progressBar = document.querySelector('.progress')
 
+  // Pause or play
   playButton.addEventListener('click', function () {
     p.getStatus(function (err, status) {
       if (err) console.error('An error ocurred while getting status of player: ' + err)
@@ -20,5 +22,10 @@ player.attach(function (err, p) {
         })
       }
     })
+  })
+
+  // Update progress bar
+  p.on('position', function () {
+    progressBar.value = p.getProgress()
   })
 })
