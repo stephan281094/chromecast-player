0f5006e9d43ca1d1498531b692d2f2602c3dd47a
stephan281094 <stephan281094@gmail.com>
2016-01-01 18:27:22 +0100

    Reattach player when window gets focussed

--- app/app.js
+++ app/app.js
@@ -1,31 +1,37 @@
 var player = require('chromecast-player')()
 
-player.attach(function (err, p) {
-  if (err) console.error('An error ocurred while attaching the player: ' + err)
-  var playButton = document.querySelector('.play')
-  var playButtonIcon = document.querySelector('.play i.fa')
-  var progressBar = document.querySelector('.progress')
+window.onfocus = init
 
-  // Pause or play
-  playButton.addEventListener('click', function () {
-    p.getStatus(function (err, status) {
-      if (err) console.error('An error ocurred while getting status of player: ' + err)
-      if (status.playerState === 'PLAYING') {
-        p.pause(function () {
-          playButtonIcon.classList.remove('fa-pause')
-          playButtonIcon.classList.add('fa-play')
-        })
-      } else {
-        p.play(function () {
-          playButtonIcon.classList.remove('fa-play')
-          playButtonIcon.classList.add('fa-pause')
-        })
-      }
+function init () {
+  player.attach(function (err, p) {
+    if (err) console.error('An error ocurred while attaching the player: ' + err)
+    var playButton = document.querySelector('.play')
+    var playButtonIcon = document.querySelector('.play i.fa')
+    var progressBar = document.querySelector('.progress')
+
+    // Pause or play
+    playButton.addEventListener('click', function () {
+      p.getStatus(function (err, status) {
+        if (err) console.error('An error ocurred while getting status of player: ' + err)
+        if (status.playerState === 'PLAYING') {
+          p.pause(function () {
+            playButtonIcon.classList.remove('fa-pause')
+            playButtonIcon.classList.add('fa-play')
+          })
+        } else {
+          p.play(function () {
+            playButtonIcon.classList.remove('fa-play')
+            playButtonIcon.classList.add('fa-pause')
+          })
+        }
+      })
+    })
+
+    // Update progress bar
+    p.on('position', function () {
+      progressBar.value = p.getProgress()
     })
   })
+}
 
-  // Update progress bar
-  p.on('position', function () {
-    progressBar.value = p.getProgress()
-  })
-})
+init()
